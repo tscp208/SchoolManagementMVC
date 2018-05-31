ae5ff163e6227f0fa04f2b656b72de75bdbc70e3
tscp208 <39762792+tscp208@users.noreply.github.com>
2018-05-31 18:12:29 +0530

    JenishJ - Checked in User Type display functionality.

---
+++ SMS/SMS_Common/Connection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace SMS_Common
+{
+    public class Connection
+    {
+        public static string dbConnection = ConfigurationManager.ConnectionStrings["dbConnection"].ConnectionString;
+    }
+}
---
+++ SMS/SMS_Entities/UserTypeEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS_Entities
+{
+    public class UserTypeEntity
+    {
+        public int UserTypeID { get; set; }
+
+        public int SrNo { get; set; }
+
+        public string UserTypeName { get; set; }
+
+        public string UserTypeDesc { get; set; }
+    }
+}
