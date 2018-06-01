ce9240e75f97b293aa2e2c7d29a56b90e28a7336
tscp208 <39762792+tscp208@users.noreply.github.com>
2018-06-01 17:50:58 +0530

    Jenish - Checked in User Type Insertion Module and other things

---
+++ SMS/SMS/Scripts/Custom/UserType/Index.js
@@ -0,0 +1,50 @@
+﻿
+$("#btnAddUserType").click(function () {
+    $.ajax({
+        type: "GET",
+        url: "/UserType/AddUserType"
+    }).done(function (response) {
+        $("#modalBody").html('');
+        $("#modalBody").html(response);
+        $("#userTypeModal").modal({
+            backdrop: 'static',
+            keyboard: false
+        });
+        $("#userTypeModal").modal('show');
+        $("form").each(function () { $.data($(this)[0], 'validator', false); });
+        $.validator.unobtrusive.parse("form");
+    });
+});
+
+function SuccessMethod(result)
+{
+    debugger;
+    if (result.Status)
+    {
+        
+        $("#userTypeModal").modal('hide');
+        $("#msgBlocks").html("<div id='divMessages' class='fade-in' role='alert'><span id='spnMsgs'></span><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>");
+        $("#spnMsgs").html(result.Message);
+        $("#divMessages").addClass("alert alert-success alert-dismissible");
+
+        $("#divMessages").fadeTo(2000, 500).slideUp(500, function () {
+            $("#divMessages").slideUp(1000);
+        });
+
+        $.ajax({
+            type: "GET",
+            url: "/UserType/UserTypeGrid"
+        }).done(function (response) {
+            $("#UserTypeGridBlock").html('');
+            $("#UserTypeGridBlock").html(response);
+        });
+    }    
+    else {
+        $("#msgBlock").html("<div id='divMessage' class='fade-in' role='alert'><span id='spnMsg'></span><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>");
+        $("#spnMsg").html(result.Message);
+        $("#divMessage").addClass("alert alert-danger alert-dismissible");
+    }
+    $("#divMessage").fadeTo(2000, 500).slideUp(500, function () {
+        $("#divMessage").slideUp(1000);
+    });
+}
---
+++ SMS/SMS_Entities/UserTypeEntity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS_Entities
+{
+    public class UserTypeEntity
+    {
+        public int UserTypeID { get; set; }
+
+        public int SrNo { get; set; }
+
+        public string UserTypeName { get; set; }
+
+        public string UserTypeDesc { get; set; }
+
+        public int CreatedBy { get; set; }
+
+        public string CreatedOn { get; set; }
+
+        public int ModifiedBy { get; set; }
+
+        public string ModifiedOn { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}
